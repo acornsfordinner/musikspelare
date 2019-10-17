b36e5d3873e800cc4f722655cb2d4f496ce1c7b3
acornsfordinner <34157140+acornsfordinner@users.noreply.github.com>
2019-10-17 18:22:19 +0200

    Add files via upload

--- music_player.js
+++ music_player.js
@@ -1,6 +1,4 @@
 
-
-
 let songNr = 1
 let queueSize
 let player = new Audio()
@@ -8,11 +6,13 @@ let player = new Audio()
 let radio = new Audio()
 radio.src = 'http://trace.dnbradio.com:8000/dnbradio_main.mp3'
 
+let shuffle = false
+
 
 function start() {
 
     /**hur många låtar finns i playlist */
-    queueSize = playlist.children.length
+    queueSize = playlist.children.length-1
 
     /**sätt den initiellt aktuella låten */
     player.src = $(".queueItem:nth-child(" + songNr + ")").attr("link")
@@ -22,9 +22,6 @@ function start() {
     $("#songInfo").text($(".queueItem:nth-child(" + songNr + ")").children(".queueSongInfo").children(".songName").text())
     $("#artistInfo").text($(".queueItem:nth-child(" + songNr + ")").children(".queueSongInfo").children(".artistName").text())
 
-    
-    /**skriv ut låtens längd*/
-    $("#timestampRight").text(player.duration)
 
     $(".queuePlayButton").css({ cursor: "pointer" })
     $(".playbackButton").css({ cursor: "pointer" })
@@ -34,20 +31,19 @@ function start() {
 
     $('.queuePlayButton').on('click', queuePlayClick)
 
+    $('#heartButton').on('click', heart_click)
 
     $('#playerPlay').on('click', play_audio)
     $('#playerPause').on('click', pause_audio)
     $('#backward').on('click', backward)
     $('#forward').on('click', forward)
-    $('#repeat').on('click', repeat)
+    $('#repeat').on('click', repeat_toggle)
+    $('#shuffle').on('click', shuffle_toggle)
     player.addEventListener('timeupdate', timestamp)
-    
-    
+    player.addEventListener('ended', song_end)
 
 }
 $(start)
-
-
 
 
 function queuePlayClick() {
@@ -59,7 +55,6 @@ function queuePlayClick() {
 
     /* hämta bilden från queue*/
     console.log($(this).parent().children(".queueCoverArt").children(".queueImg").attr("src"))
-
 
     /*spara bilden över bilden i player*/
     $("#coverArt").attr("src", $(this).parent().children(".queueCoverArt").children(".queueImg").attr("src"))
@@ -73,34 +68,50 @@ function queuePlayClick() {
     //hämta och skriv över texten
     $("#songInfo").text($(this).parent().children(".queueSongInfo").children(".songName").text())
     $("#artistInfo").text($(this).parent().children(".queueSongInfo").children(".artistName").text())
-    
 
-    
+
+
     /*spela ljud, från början */
     player.currentTime = 0
-    player.play()    
-    
+    player.play()
+
 }
 
 
 function timestamp(event) {
-    console.log("omg", Math.round(event.timeStamp/1000), player.duration)
+    //console.log("omg", Math.round(event.timeStamp / 1000), player.duration)
+    console.log("progress", Math.round(player.currentTime),player.duration)
+    if (isNaN(player.duration)) {
 
+    }
+    if (!isFinite(player.duration)){
+        $('#timestampLeft').text("")
+        $('#timestampRight').text("")
+        
+    }
+
+    else {
     $('#timestampLeft').text(sec_to_min(Math.round(player.currentTime)))
-    
+    $("#timestampRight").text(sec_to_min(Math.round(player.duration)))
+    }
+
+    /** Popup om att ha lystnat för länge*/
+    let woke
+    if (Math.round(player.currentTime) == 60){
+        if (!confirm("Vill du fortsätta lyssna?")){
+            player.pause()
+        }
+    }
+    $("#bar").css({ width: ((player.currentTime/player.duration)*100) + "%" })
 }
 
-function sec_to_min(sec){
-    let min = Math.floor(sec/60)
-    sec = sec%60
-    
-    if (min<10){
-        min = "0"+min
-    }
-    if (sec<10){
-        sec = "0"+sec
-    }
-    return min+":"+sec
+function sec_to_min(sec) {
+    let min = Math.floor(sec / 60)
+    sec = sec % 60
+
+    if (min < 10) { min = "0" + min }
+    if (sec < 10) { sec = "0" + sec }
+    return min + ":" + sec
 
 }
 
@@ -108,7 +119,12 @@ function play_audio() {
     console.log('audio playing')
 
     player.play()
+    update_timestamp()
+}
+
+function update_timestamp() {
     /**skriv ut låtens längd*/
+    console.log('update timestamp')
     $("#timestampRight").text(sec_to_min(Math.round(player.duration)))
     console.log(sec_to_min(Math.round(player.duration)))
 }
@@ -132,6 +148,7 @@ function backward() {
 
     if (player.currentTime < 2) {
         previous_song()
+
     }
 
     player.currentTime = 0
@@ -142,74 +159,127 @@ function backward() {
 }
 
 function forward() {
-    let running = false
-
-    if (player.paused == false) {
-        running = true
-    }
+    let paused = player.paused
     next_song()
 
-    if (running == true) {
-        play_audio()
+    play_audio()
+
+    if (paused == true) {
+        pause_audio()
     }
-
 }
-
 
 function previous_song() {
 
-    let increment = songNr - 1
-    if (increment < 1) {
-        increment = queueSize
+    let newSongNr = songNr - 1
+    if (newSongNr < 1) {
+        newSongNr = queueSize
     }
     /**avmarkera gammal låt i queue */
     $(".queueItem:nth-child(" + songNr + ")").css({ backgroundColor: "transparent" })
     /**markera ny låt i queue */
-    $(".queueItem:nth-child(" + increment + ")").css({ backgroundColor: "orange" })
+    $(".queueItem:nth-child(" + newSongNr + ")").css({ backgroundColor: "orange" })
     /**hämta infotexten */
-    $("#songInfo").text($(".queueItem:nth-child(" + increment + ")").children(".queueSongInfo").children(".songName").text())
-    $("#artistInfo").text($(".queueItem:nth-child(" + increment + ")").children(".queueSongInfo").children(".artistName").text())
+    $("#songInfo").text($(".queueItem:nth-child(" + newSongNr + ")").children(".queueSongInfo").children(".songName").text())
+    $("#artistInfo").text($(".queueItem:nth-child(" + newSongNr + ")").children(".queueSongInfo").children(".artistName").text())
     /** hämta bilden från queue, spara som coverArt*/
-    $("#coverArt").attr("src", $(".queueItem:nth-child(" + increment + ")").children(".queueCoverArt").children(".queueImg").attr("src"))
+    $("#coverArt").attr("src", $(".queueItem:nth-child(" + newSongNr + ")").children(".queueCoverArt").children(".queueImg").attr("src"))
 
 
     /**spara aktivt låtnummer som källa */
-    player.src = $(".queueItem:nth-child(" + increment + ")").attr("link")
+    player.src = $(".queueItem:nth-child(" + newSongNr + ")").attr("link")
 
-    
+
     /**spara aktuell låt till songNr */
-    songNr = increment
+    songNr = newSongNr
 }
 
 
 function next_song() {
-
-    let increment = songNr + 1
-    if (increment > queueSize) {
-        increment = 1
+    if (shuffle == true) {
+        next_song_shuffle()
     }
-    /**avmarkera gammal låt i queue */
-    $(".queueItem:nth-child(" + songNr + ")").css({ backgroundColor: "transparent" })
-    /**markera ny låt i queue */
-    $(".queueItem:nth-child(" + increment + ")").css({ backgroundColor: "orange" })
-    /**hämta infotexten */
-    $("#songInfo").text($(".queueItem:nth-child(" + increment + ")").children(".queueSongInfo").children(".songName").text())
-    $("#artistInfo").text($(".queueItem:nth-child(" + increment + ")").children(".queueSongInfo").children(".artistName").text())
-    /* hämta bilden från queue, spara som coverArt*/
-    $("#coverArt").attr("src", $(".queueItem:nth-child(" + increment + ")").children(".queueCoverArt").children(".queueImg").attr("src"))
+
+    else {
+        let newSongNr = songNr + 1
+        if (newSongNr > queueSize) {
+            newSongNr = 1
+        }
+        /**avmarkera gammal låt i queue */
+        $(".queueItem:nth-child(" + songNr + ")").css({ backgroundColor: "transparent" })
+        /**markera ny låt i queue */
+        $(".queueItem:nth-child(" + newSongNr + ")").css({ backgroundColor: "orange" })
+        /**hämta infotexten */
+        $("#songInfo").text($(".queueItem:nth-child(" + newSongNr + ")").children(".queueSongInfo").children(".songName").text())
+        $("#artistInfo").text($(".queueItem:nth-child(" + newSongNr + ")").children(".queueSongInfo").children(".artistName").text())
+        /* hämta bilden från queue, spara som coverArt*/
+        $("#coverArt").attr("src", $(".queueItem:nth-child(" + newSongNr + ")").children(".queueCoverArt").children(".queueImg").attr("src"))
 
 
-    /*spara aktivt låtnummer som källa */
-    player.src = $(".queueItem:nth-child(" + increment + ")").attr("link")
-    
-    /**spara aktuell låt till songNr */
-    songNr = increment
+        /*spara aktivt låtnummer som källa */
+        player.src = $(".queueItem:nth-child(" + newSongNr + ")").attr("link")
 
+        /**spara aktuell låt till songNr */
+        songNr = newSongNr
+    }
 }
 
+function repeat_toggle() {
+    //funkar inte, varför?
+    //player.loop = true|false   
+    
+    if (player.loop==true){
+        player.loop=false
+    }
+    else{
+        player.loop=true
+    }
+    console.log("repeat "+player.loop)
+    
+}
 
-function repeat() {
-    player.loop = true | false
-    console.log("loop toggled")
+function shuffle_toggle() {
+    shuffle == true ? shuffle = false : shuffle = true
+    console.log("shuffle is now " + shuffle)
+}
+function next_song_shuffle() {
+    if (shuffle == true) {
+        let newSongNr
+        do {
+            newSongNr = Math.ceil(Math.random() * queueSize)
+        } while (newSongNr == songNr)
 
+        $(".queueItem:nth-child(" + songNr + ")").css({ backgroundColor: "transparent" })
+        /**markera ny låt i queue */
+        $(".queueItem:nth-child(" + newSongNr + ")").css({ backgroundColor: "orange" })
+        /**hämta infotexten */
+        $("#songInfo").text($(".queueItem:nth-child(" + newSongNr + ")").children(".queueSongInfo").children(".songName").text())
+        $("#artistInfo").text($(".queueItem:nth-child(" + newSongNr + ")").children(".queueSongInfo").children(".artistName").text())
+        /* hämta bilden från queue, spara som coverArt*/
+        $("#coverArt").attr("src", $(".queueItem:nth-child(" + newSongNr + ")").children(".queueCoverArt").children(".queueImg").attr("src"))
+
+
+        /*spara aktivt låtnummer som källa */
+        player.src = $(".queueItem:nth-child(" + newSongNr + ")").attr("link")
+
+        /**spara aktuell låt till songNr */
+        songNr = newSongNr
+    }
+}
+function song_end() {
+    if (player.loop == true) {
+        player.currentTime = 0
+    }
+    else if (shuffle == true) {
+        next_song_shuffle()   
+    }
+    else {
+        next_song()
+    }
+    play_audio()
+}
+
+function heart_click(){
+    //TO DO
+    //change font color
 }
